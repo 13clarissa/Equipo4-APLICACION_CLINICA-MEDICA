ae47cf0e3f1f74ac1b11ab8d823dbff6ecbf1404
13clarissa <clarissa.corea@unah.hn>
2022-11-30 01:13:57 -0600

    Implementación de Usuario
    Desarrollo de la implementación de Usuarios

---
+++ Aplicacion_Para_Clinica_Medica/Clinica_Medica/Pages/MisUsuarios/NuevoUsuario.razor.cs
@@ -0,0 +1,50 @@
+﻿using Clinica_Medica.Interfaces;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Clinica_Medica.Pages.MisUsuarios
+{
+	public partial class NuevoUsuario
+	{
+		[Inject] private IUsuarioService usuarioService { get; set; }
+		[Inject] private NavigationManager navigationManager { get; set; }
+		private Usuario user = new Usuario();
+
+		[Inject] SweetAlertService Swal { get; set; }
+
+		protected async void Guardar()
+		{
+			if (string.IsNullOrEmpty(user.Codigo) || string.IsNullOrEmpty(user.Nombre)
+				|| string.IsNullOrEmpty(user.Clave) || string.IsNullOrEmpty(user.Rol) || user.Rol == "Seleccionar")
+			{
+				return;
+			}
+
+			bool inserto = await usuarioService.Nuevo(user);
+
+			if (inserto)
+			{
+				await Swal.FireAsync("Felifidades", "Usuario Guardado", SweetAlertIcon.Success);
+			}
+			else
+			{
+				await Swal.FireAsync("Error", "No se pudo guardar el usuario", SweetAlertIcon.Error);
+			}
+
+			navigationManager.NavigateTo("/Usuarios");
+		}
+
+		protected void Cancelar()
+		{
+			navigationManager.NavigateTo("/Usuarios");
+		}
+	}
+}
+
+enum Roles
+{
+	Seleccionar,
+	Administrador,
+	Usuario
+}
---
+++ Aplicacion_Para_Clinica_Medica/Clinica_Medica/Pages/MisUsuarios/Usuarios.razor.cs
@@ -0,0 +1,18 @@
+﻿using Clinica_Medica.Interfaces;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Clinica_Medica.Pages.MisUsuarios
+{
+    public partial class Usuarios
+    {
+        [Inject] private IUsuarioService usuarioService { get; set; }
+
+        private IEnumerable<Usuario> lista { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            lista = await usuarioService.GetLista();
+        }
+    }
+}
