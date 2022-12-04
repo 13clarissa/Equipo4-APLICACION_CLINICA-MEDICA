256bc35cb426c3a33c5d3ea552e372a9588e44b3
KatherineCalix <113080728+KatherineCalix@users.noreply.github.com>
2022-12-04 01:07:46 -0600

    Nueva Factura
    terminación de clase 31 y parte del 32

--- Aplicacion_Para_Clinica_Medica/Clinica_Medica/Pages/Facturacion/NuevaFactura.razor.cs
+++ Aplicacion_Para_Clinica_Medica/Clinica_Medica/Pages/Facturacion/NuevaFactura.razor.cs
@@ -1,6 +1,25 @@
-﻿namespace Clinica_Medica.Pages.Facturacion
+﻿using Clinica_Medica.Interfaces;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Microsoft.AspNetCore.Components;
+using Modelos;
+
+namespace Clinica_Medica.Pages.Facturacion
 {
     public partial class NuevaFactura
     {
+        [Inject] private IFacturaService facturaService { get; set; }
+        [Inject] private IDetalleFacturaService detalleFacturaService { get; set; }
+        [Inject] private IServiciosService productoService { get; set; }
+        [Inject] private SweetAlertService Swal { get; set; }
+        [Inject] NavigationManager _navigationManager { get; set; }
+        [Inject] private IHttpContextAccessor httpContextAccessor { get; set; }
+
+        private Factura factura = new Factura();
+        private List<DetalleFactura> listaDetalleFactura = new List<DetalleFactura>();
+        private Servicio servicio = new Servicio();
+
+        private int cantidad { get; set; }
+        private string codigoServicio { get; set; }
+
     }
 }
